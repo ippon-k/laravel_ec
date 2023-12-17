eb56b2d3356e97c3cf32a032ff36c9b34d34e445
ippon <mentaikodondon@gmail.com>
2023-12-17 15:55:25 +0900

    nameSlot

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -18,6 +18,9 @@
 
     </head>
     <body class="font-sans text-gray-900 antialiased">
+        <header>
+            {{ $header }}
+        </header>
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
