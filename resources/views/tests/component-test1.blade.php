3924365c2a8d36a463eddabedbe13dc1f237efc3
ippon <mentaikodondon@gmail.com>
2023-12-17 19:26:57 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
   <x-slot name="header">header1</x-slot>
 コンポーネントテスト１
   <x-tests.card title="タイトルを渡すよ" content="本文を渡すよ" :message="$message"/>
+  <x-tests.card title="タイトル2" />
 </x-tests.app>
 {{-- コロンをつけると属性ではなく変数を受け取る --}}
