93bf98fc63eed82998b3b0bfeb8420c30140ee4f
ippon <mentaikodondon@gmail.com>
2023-12-23 19:04:56 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,6 @@
 コンポーネントテスト１
   <x-tests.card title="タイトルを渡すよ" content="本文を渡すよ" :message="$message"/>
   <x-tests.card title="タイトル2" />
+  <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.app>
 {{-- コロンをつけると属性ではなく変数を受け取る --}}
